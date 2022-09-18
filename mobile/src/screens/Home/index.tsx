8a2533efb06afa295ff306bd5b94eccd9db03b07
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2022-09-18 11:52:08 -0300

    Aula 3 : Nao finalizada

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,27 +1,37 @@
-import { View, Image, FlatList } from "react-native";
-
+import { Image, FlatList } from "react-native";
+import { useEffect, useState } from "react";
 import { styles } from "./styles";
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
-import { GameCard } from "../../components/GameCard";
-import { GAMES } from "../../utils/games";
+import { GameCard, GameCardProps } from "../../components/GameCard";
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://192.168.0.111:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.contentList}
       />
-    </View>
+    </SafeAreaView>
   );
 }
